6a1bf51088e241161cb17cabd816453c94f65f52
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2023-11-03 12:52:43 +0530

    Login

---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Landingpage from './Landingpage'
+
+function Homepage() {
+  return (
+    <div>
+        <Navbar/>
+        <Landingpage/>
+    </div>
+  )
+}
+
+export default Homepage
